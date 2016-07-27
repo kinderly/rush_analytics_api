6ff7446e4d49a70b35f9ed7ef38285d07c8104d6
Sergey Pustovalov <pustserg@yandex.ru>
2016-07-27 18:09:58 +0300

    Added quotations project parser

--- lib/rush_analytics_api.rb
+++ lib/rush_analytics_api.rb
@@ -4,6 +4,7 @@ module RushAnalyticsApi
   require 'rush_analytics_api/create_wordstat_project'
   require 'rush_analytics_api/wordstat_project_status'
   require 'rush_analytics_api/wordstat_project_result_parser'
+  require 'rush_analytics_api/quotation_project_result_parser'
 
   require 'rush_analytics_api/create_top10_project'
   require 'rush_analytics_api/top10_project_status'

665d6a21c268590a8365c1485b5eb308e5896dd5
Sergey Pustovalov <pustserg@yandex.ru>
2016-09-13 16:31:12 +0300

    Do not verfiy ssl

--- lib/rush_analytics_api/wordstat_project_result_parser.rb
+++ lib/rush_analytics_api/wordstat_project_result_parser.rb
@@ -5,7 +5,7 @@ module RushAnalyticsApi
     end
 
     def call
-      tmp_file = open(@url)
+      tmp_file = open(@url, ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE)
       file = Roo::Spreadsheet.open(tmp_file, extension: 'xlsx')
       sheet = file.sheet(0)
       sheet.parse(word: sheet.row(1)[0], count: sheet.row(1)[1])[1..-1]

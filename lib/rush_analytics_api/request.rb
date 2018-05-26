af2e6c537928127f074911f1b0f4080fc44cd8ce
Andrey Stikheev <andrey.stikheev@gmail.com>
2018-05-26 11:09:12 +0300

    Переходим на https

--- lib/rush_analytics_api/request.rb
+++ lib/rush_analytics_api/request.rb
@@ -1,6 +1,6 @@
 module RushAnalyticsApi
   class Request
-    URL = 'http://api.rush-analytics.ru/api.php?wsdl'
+    URL = 'https://api.rush-analytics.ru/api.php?wsdl'
 
     def initialize(action, api_key = nil, message = {})
       @action = action
